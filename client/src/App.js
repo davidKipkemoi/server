f12ffebce3f8ece32a636be97c1fb208b2c7555d
Kiprich <davidkipkemoi30@gmail.com>
2023-08-10 00:11:17 +0300

    new

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import ChatContainer from "./components/ChatContainer";
+function App() {
+  return (
+  
+   
+   
+    <div style={{background:"black"}}>
+    
+      <ChatContainer/>
+   </div>
+ 
+  );
+}
+
+export default App;
